ab7a0fe2bd0665f669fb4b07951e7cee901ee0f5
tio1234 <oktrioka.gisbu12@gmail.com>
2020-11-25 19:06:03 +0700

    Update

--- app/src/main/java/com/example/proyekcapstone/MainActivity.java
+++ app/src/main/java/com/example/proyekcapstone/MainActivity.java
@@ -1,36 +1,102 @@
 package com.example.proyekcapstone;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.TextView;
 
-import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.berita.R;
+import com.google.android.material.card.MaterialCardView;
 
-    private ArrayList<RecyclerModel> beritaArray;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    MaterialCardView cvBusiness;
+    TextView tvToday;
+    String hariIni;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        addData();
+        cvBusiness = findViewById(R.id.cardBusiness);
+        cvBusiness.setOnClickListener(this);
+        tvToday = findViewById(R.id.tvDate);
+        Date dateNow = Calendar.getInstance().getTime();
+        hariIni = (String) DateFormat.format("EEEE", dateNow);
+        if (hariIni.equalsIgnoreCase("sunday")) {
+            hariIni = "Minggu";
+        } else if (hariIni.equalsIgnoreCase("monday")) {
+            hariIni = "Senin";
+        } else if (hariIni.equalsIgnoreCase("tuesday")) {
+            hariIni = "Selasa";
+        } else if (hariIni.equalsIgnoreCase("wednesday")) {
+            hariIni = "Rabu";
+        } else if (hariIni.equalsIgnoreCase("thursday")) {
+            hariIni = "Kamis";
+        } else if (hariIni.equalsIgnoreCase("friday")) {
+            hariIni = "Jumat";
+        } else if (hariIni.equalsIgnoreCase("saturday")) {
+            hariIni = "Sabtu";
+        }
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        RecyclerAdapter adapter = new RecyclerAdapter(beritaArray);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(MainActivity.this);
-        recyclerView.setLayoutManager(layoutManager);
-        recyclerView.setAdapter(adapter);
+        getToday();
+
     }
 
-    void addData(){
-        beritaArray = new ArrayList<>();
-        beritaArray.add(new RecyclerModel("Konflik Agraria", "20-09-2020", "Hukum"));
-        beritaArray.add(new RecyclerModel("Capres 2020", "21-09-2020", "Politik"));
-        beritaArray.add(new RecyclerModel("Barcelona VS Real Madrid", "22-09-2020", "Sepakbola"));
-        beritaArray.add(new RecyclerModel("Pemerintah janjikan PLN Gratis", "23-09-2020", "Ekonomi"));
+    private void getToday() {
+        Date date = Calendar.getInstance().getTime();
+        String tanggal = (String) DateFormat.format("d", date);
+        String monthNumber = (String) DateFormat.format("M", date);
+        String year = (String) DateFormat.format("yyyy", date);
+
+        int month = Integer.parseInt(monthNumber);
+        String bulan = null;
+        if (month == 1) {
+            bulan = "Januari";
+        } else if (month == 2) {
+            bulan = "Februari";
+        } else if (month == 3) {
+            bulan = "Maret";
+        } else if (month == 4) {
+            bulan = "April";
+        } else if (month == 5) {
+            bulan = "Mei";
+        } else if (month == 6) {
+            bulan = "Juni";
+        } else if (month == 7) {
+            bulan = "Juli";
+        } else if (month == 8) {
+            bulan = "Agustus";
+        } else if (month == 9) {
+            bulan = "September";
+        } else if (month == 10) {
+            bulan = "Oktober";
+        } else if (month == 11) {
+            bulan = "November";
+        } else if (month == 12) {
+            bulan = "Desember";
+        }
+        String formatFix = hariIni + ", " + tanggal + " " + bulan + " " + year;
+        tvToday.setText(formatFix);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.cardBusiness) {
+            Intent intent = new Intent(MainActivity.this, BusinessActivity.class);
+            startActivity(intent);
+
+
+        }
+
     }
 }
